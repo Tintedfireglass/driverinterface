2b0f31be7af5d5cfa20f9801cf2028b38eca3642
Tanmay Paturu <tintedfireglass@hotmail.com>
2024-04-09 20:23:13 +0530

    basic framework code added

---
+++ interface.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import NumericProperty, StringProperty
+from kivy.clock import Clock
+from datetime import datetime
+
+class CarDashboard(FloatLayout):
+    accelerator_pedal = NumericProperty(0)
+    soc = NumericProperty(90)
+    cell_temperature = NumericProperty(25)
+    current_time = StringProperty("")
+
+    def __init__(self, **kwargs):
+        super(CarDashboard, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update_time, 1)
+
+    def update_time(self, dt):
+        self.current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+class CarDash(App):
+    def build(self):
+        return CarDashboard()
+
+if __name__ == '__main__':
+    CarDash().run()
