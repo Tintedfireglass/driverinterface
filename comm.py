175081f66166da963ade6a669dc807ebbf61f2ca
Tanmay Paturu <tintedfireglass@hotmail.com>
2024-05-18 19:11:49 +0530

    stm uart files added

--- comm.py
+++ comm.py
@@ -1,9 +1,8 @@
 from serial import Serial
 
 uart_read():
-    with Serial('/dev/ttyUSB0', 9600) as serial:
-        serial.send('Apple pie')
-        answer = serial.readline()
-        return answer
+    with Serial('/dev/ttyUSB0', 115200) as serial:
+        dataa = serial.readline()
+        return dataa
         
 
