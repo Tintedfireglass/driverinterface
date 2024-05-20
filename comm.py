e419e701218c392db309aac0e360cdbf40080f49
Tanmay Paturu <tintedfireglass@hotmail.com>
2024-05-20 15:21:07 +0530

    serial1

--- comm.py
+++ comm.py
@@ -1,7 +1,7 @@
 from serial import Serial
 import time
 while True:
-    with Serial('/dev/ttyUSB0', 115200) as serial:
+    with Serial('/dev/ttyAMA0', 115200) as serial:
         dataa = serial.readline()
         print(dataa)
         time.sleep(1)
