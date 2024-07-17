0f15e4cc64fb3b779c1df24ab527cefe7823b5c4
Tanmay Paturu <tintedfireglass@hotmail.com>
2024-07-17 21:56:43 +0530

    add ht

--- interface.py
+++ interface.py
@@ -84,6 +84,8 @@ class CarDashboard(FloatLayout):
 class CarDashboardApp(App):
     def build(self):
         Config.set('graphics','width','800')
+        Config.set('graphics','height','480')
+
         return CarDashboard()
 
 
