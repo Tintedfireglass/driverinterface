726de81eacdc14e5d0c871a60c71a26be18b2f0f
Tanmay Paturu <tintedfireglass@hotmail.com>
2024-07-24 12:01:52 +0530

    Add extra widgets for new requirements

--- interface.py
+++ interface.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import NumericProperty, StringProperty
 from kivy.clock import Clock
-from kivy.graphics import Color, Line, Rectangle, PushMatrix, PopMatrix, Rotate
+from kivy.graphics import Color, Line, Rectangle
 from kivy.uix.image import Image
 from math import cos, sin, pi
 from kivy.core.window import Window
@@ -17,14 +17,14 @@ class Speedometer(FloatLayout):
         super(Speedometer, self).__init__(**kwargs)
         self.size_hint = (None, None)
         self.size = (400, 400)
-        self.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+        self.pos_hint = {'center_x': 0.3, 'center_y': 0.5}  # Adjusted for speedometer position
 
-        # Add background image
+        # Background image
         self.bg_image = Image(source='speedometer.png', size=self.size, pos=self.pos)
         self.add_widget(self.bg_image)
 
-        # Add speedometer label for value display
-        self.speed_label = Label(font_size=20, pos=(self.center_x - 10, self.center_y - 30))
+        # Speedometer label for value display
+        self.speed_label = Label(font_size=20, color=(1, 1, 1, 1))
         self.add_widget(self.speed_label)
 
         self.bind(size=self.update_canvas, pos=self.update_canvas)
@@ -55,18 +55,18 @@ class BatteryIndicator(FloatLayout):
     
     def __init__(self, **kwargs):
         super(BatteryIndicator, self).__init__(**kwargs)
-        self.size = (100, 150)  # Adjust the size to accommodate text and bar
-        self.pos = (50, self.height - 200)  # Adjust the position
-        self.label = Label(text=str(self.soc) + '%', font_size=20, pos=(self.center_x, self.top - 20))
-        self.add_widget(self.label)
+        self.size_hint = (None, None)
+        self.size = (100, 150) 
+        self.pos_hint = {'x': 0.05, 'top': 0.95}  
         self.update_battery()
+        self.bind(soc=self.update_battery)
 
-    def update_battery(self):
+    def update_battery(self, *args):
         self.canvas.clear()
         with self.canvas:
             # Battery outline
             Color(0.5, 0.5, 0.5)  
-            Rectangle(pos=self.pos, size=(self.width, self.height))
+            Rectangle(pos=self.pos, size=self.size)
 
             # Battery terminal
             Color(0.5, 0.5, 0.5)
@@ -80,21 +80,32 @@ class BatteryIndicator(FloatLayout):
             Rectangle(pos=(self.pos[0] + 5, self.pos[1] + 10), size=(self.width - 10, self.height - 30))
 
             # SOC level
+            soc_height = (self.height - 30) * (self.soc / 100.0)
             if self.soc > 50:
                 Color(0, 1, 0)  # Green color
             elif self.soc > 20:
                 Color(1, 1, 0)  # Yellow color
             else:
                 Color(1, 0, 0)  # Red color
-            soc_height = (self.height - 30) * (self.soc / 100.0)
             Rectangle(pos=(self.pos[0] + 5, self.pos[1] + 10), size=(self.width - 10, soc_height))
 
-        # Update the text label
-        self.label.text = str(self.soc) + '%'
-        self.label.pos = (self.center_x - self.label.width / 2, self.top - 25)
+class HorizontalBar(FloatLayout):
+    value = NumericProperty(0)
+    label_text = StringProperty('')
 
-    def on_soc(self, instance, value):
-        self.update_battery()
+    def __init__(self, **kwargs):
+        super(HorizontalBar, self).__init__(**kwargs)
+        self.size_hint = (None, None)
+        self.size = (300, 50)
+        self.bind(value=self.update_canvas)
+
+    def update_canvas(self, *args):
+        self.canvas.clear()
+        with self.canvas:
+            Color(0.5, 0.5, 0.5)
+            Rectangle(pos=self.pos, size=self.size)
+            Color(0, 1, 0)
+            Rectangle(pos=self.pos, size=(self.size[0] * (self.value / 100.0), self.size[1]))
 
 class CarDashboard(FloatLayout):
     accelerator_pedal = NumericProperty(0)
@@ -107,37 +118,74 @@ class CarDashboard(FloatLayout):
         self.speedometer = Speedometer()
         self.add_widget(self.speedometer)
 
+        # RPM Meter (similar to Speedometer)
+        self.rpm_meter = Speedometer()
+        self.rpm_meter.pos_hint = {'center_x': 0.7, 'center_y': 0.5}  # Adjusted for RPM meter position
+        self.add_widget(self.rpm_meter)
+
         # Battery Indicator (top-left)
-        self.battery_indicator = BatteryIndicator(size_hint=(None, None), size=(150, 150), pos_hint={"left": 0.1, "top": 0.9})
+        self.battery_indicator = BatteryIndicator(size_hint=(None, None), size=(150, 150), pos_hint={"x": 0.05, "top": 0.85})
         self.add_widget(self.battery_indicator)
 
+        # Battery SOC Label
+        self.battery_soc_label = Label(text="SOC: 100%", font_size=20, color=(1, 1, 1, 1), pos_hint={"x": -0.4225, "top": 1.4})
+        self.add_widget(self.battery_soc_label)
+
         # Cell Temperature Label (top-right)
-        self.cell_temperature_label = Label(text=str(self.cell_temperature) + '°C', font_size=20, size_hint=(None, None), size=(self.width / 2, 50), pos_hint={"right": 0.95, "top": 0.95}, halign='right', valign='middle')
+        self.cell_temperature_label = Label(text=str(self.cell_temperature) + '°C', font_size=20, color=(1, 1, 1, 1),
+                                            size_hint=(None, None), size=(self.width / 2, 50), pos_hint={"right": 0.95, "top": 0.95},
+                                            halign='right', valign='middle')
         self.add_widget(self.cell_temperature_label)
 
         # Error Message Label (bottom-left)
-        self.error_message_label = Label(text=self.error_message, font_size=20, size_hint=(None, None), size=(self.width / 2, 50), pos_hint={"left": 0.05, "bottom": 0.05}, halign='left', valign='middle')
+        self.error_message_label = Label(text=self.error_message, font_size=20, color=(1, 1, 1, 1),
+                                         size_hint=(None, None), size=(self.width / 2, 50), pos_hint={"left": 0.05, "bottom": 0.05},
+                                         halign='left', valign='middle')
         self.add_widget(self.error_message_label)
 
         # Ojas Image (bottom-right)
         self.logo_image = Image(source='logo.png', size_hint=(None, None), size=(200, 200), pos_hint={"right": 0.95, "bottom": 0.05})
         self.add_widget(self.logo_image)
 
-        self.dummy_soc = 100  # Initial SOC value
-        self.dummy_value = 0  # Initial speed value
+        # Power and Current Bars
+        self.power_bar = HorizontalBar(pos_hint={"center_x": 0.4, "top": 0.85}, label_text='Power: ')
+        self.current_bar = HorizontalBar(pos_hint={"center_x": 0.6, "top": 0.85}, label_text='Current: ')
+        self.add_widget(self.power_bar)
+        self.add_widget(self.current_bar)
 
-        Clock.schedule_interval(self.update_speedometer, 0.1)
+        # Power Label
+        self.power_label = Label(text="Power: 0", font_size=20, color=(1, 1, 1, 1), pos_hint={"center_x": 0.4, "top": 1.325})
+        self.add_widget(self.power_label)
 
-    def update_speedometer(self, dt):
+        # Current Label
+        self.current_label = Label(text="Current: 0", font_size=20, color=(1, 1, 1, 1), pos_hint={"center_x": 0.6, "top": 1.325})
+        self.add_widget(self.current_label)
+
+        self.dummy_soc = 100  
+        self.dummy_value = 0  
+        self.dummy_rpm = 0  
+        self.dummy_power = 0  
+        self.dummy_current = 0  
+
+        Clock.schedule_interval(self.update_dashboard, 0.1)
+
+    def update_dashboard(self, dt):
         # Update dummy data
         self.dummy_soc = max(0, self.dummy_soc - 0.1)  
         self.dummy_value = min(100, self.dummy_value + 0.5)  
+        self.dummy_rpm = min(8000, self.dummy_rpm + 50)  
+        self.dummy_power = (self.dummy_power + 1) % 101  
+        self.dummy_current = (self.dummy_current + 2) % 101  
 
         self.speedometer.soc = int(self.dummy_soc)
         self.speedometer.value = int(self.dummy_value)
-        maxim = 360
-        self.speedometer.movement = -180 - (self.speedometer.value / maxim) * 360  
+        
+        self.speedometer.movement = -180 - (self.speedometer.value / 360) * 360  
         self.speedometer.draw_speedometer()
+
+        self.rpm_meter.value = int(self.dummy_rpm)
+        self.rpm_meter.movement = -180 - (self.rpm_meter.value / 8000) * 360  # Assuming 8000 RPM is the max
+        self.rpm_meter.draw_speedometer()
 
         color_value = max(min((1 - self.speedometer.value / 100) * 2, 1), 0)
         self.canvas.before.clear()
@@ -146,17 +194,24 @@ class CarDashboard(FloatLayout):
             Rectangle(pos=self.pos, size=self.size)
 
         self.battery_indicator.soc = self.speedometer.soc
+        self.battery_soc_label.text = "SOC: " + str(self.battery_indicator.soc) + '%'
 
         self.cell_temperature = 30  
         self.cell_temperature_label.text = str(self.cell_temperature) + '°C'
 
+        self.power_bar.value = self.dummy_power
+        self.current_bar.value = self.dummy_current
+
+        self.power_label.text = "Power: " + str(self.power_bar.value)
+        self.current_label.text = "Current: " + str(self.current_bar.value)
+
         # Update the error message
         self.error_message = self.get_error_message()
-        self.error_message_label.text = 'Error: ' + self.error_message
+        self.error_message_label.text = '       Error: ' + self.error_message
 
     def get_error_message(self):
-        self.errrr = "E5 Pedal Error!"
-        return "       "+self.errrr
+        self.errrr = "E5"
+        return self.errrr
 
 class CarDashboardApp(App):
     def build(self):
